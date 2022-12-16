9608cd8ef3237adb850bc5fb1685c2ee36cac887
HiIAmTzeKean <ngtzekean600@gmail.com>
2022-12-16 17:24:06 +0800

    app data

---
+++ api/public/data.js
@@ -0,0 +1,13 @@
+const Person =  {
+	"0" : {"name": "Larry", "points": 0},
+	"1" : {"name": "John", "points": 0},
+	"2" : {"name": "Peter", "points": 0}
+}
+
+
+const Schedule = {
+	"2022" :[0,1],
+	"2023" : [1,2]
+}
+
+module.exports = {Person, Schedule}
